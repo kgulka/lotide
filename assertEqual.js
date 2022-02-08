b3bd3447a875f75c33d33c8aecc31b1be524608a
Kent Gulka <kentgulka@modindsoft.com>
2022-02-08 03:31:23 +0000

    new assert function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+//Function assertEqual
+const assertEqual = function(actual, expected) {
+  const emoji = require('node-emoji');
+  const pass = emoji.get('smiley');
+  const fail = emoji.get('🛑');
+  
+  if (actual === expected) {
+    console.log(pass + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(fail + " Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//Test Code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
