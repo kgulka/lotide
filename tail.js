a66d1dd41ea70fac0bbc8d614fbc12c143ed80c7
Kent Gulka <kentgulka@modindsoft.com>
2022-02-08 04:45:43 +0000

    added tail

---
+++ tail.js
@@ -0,0 +1,28 @@
+//function tail
+const tail = function(arr) {
+  return arr.slice(1);
+};
+//Function assertEqual
+const assertEqual = function(actual, expected) {
+  const emoji = require('node-emoji');
+  const pass = emoji.get('smiley');
+  const fail = emoji.get('🛑');
+  
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Test Code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+
+assertEqual(1,1);
+ 
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
