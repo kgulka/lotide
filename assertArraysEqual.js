029c02358f913ff27882d93141056b88ff0d25a3
Kent Gulka <kentgulka@modindsoft.com>
2022-02-09 02:44:10 +0000

    inital assert Arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,44 @@
+//Function assertEqual
+const assertEqual = function(actual, expected) {
+  const emoji = require('node-emoji');
+  const pass = emoji.get('smiley');
+  const fail = emoji.get('🛑');
+  
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if ((arr1[i] !== arr2[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const emoji = require('node-emoji');
+  const pass = emoji.get('smiley');
+  const fail = emoji.get('🛑');
+  
+  if (eqArrays(actual, expected)) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//Test Code
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+assertArraysEqual([1, 2, 3], [1, "2", 3]);
+
+assertArraysEqual([1, 2, 3], [2, 2, 3]);
