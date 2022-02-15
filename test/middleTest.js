044177c0f82f183e7ed6c876dd240db68c8e48bb
Kent Gulka <kentgulka@modindsoft.com>
2022-02-15 03:43:02 +0000

    convert tests to use Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,24 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require('../middle');
+const assert = require('chai').assert;
 
 //Test Code
-//console.log("1st test:",middle([1, 2, 3]));
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("returns 2,3 for [1,2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 3 for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns 2,3 for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns 3,4 for [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });  
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -14,7 +14,7 @@ const assert = require('chai').assert;
 //tail(words); // no need to capture the return value since we are not checking it
 //assertEqual(words.length, 3); // original array should still have 3 elements!
 
-describe("#head", () => {
+describe("#tail", () => {
   it("returns 2,3 for [1,2,3]", () => {
     assert.deepEqual(tail([1, 2, 3]),[2, 3]);
   });
