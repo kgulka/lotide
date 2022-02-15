466ab7dbb7c81f9f0295d643009f6d89a5e2fa0c
Kent Gulka <kentgulka@modindsoft.com>
2022-02-15 00:10:50 +0000

    refactored to be modular and use test file

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
-    return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if ((arr1[i] !== arr2[i])) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const emoji = require('node-emoji');
-  const pass = emoji.get('smiley');
-  const fail = emoji.get('🛑');
-  
-  if (eqArrays(actual, expected)) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(arrIn) {
   //return the middle of an array.
   //this means a single element in an odd numbered array or
@@ -46,15 +23,5 @@ const middle = function(arrIn) {
   return arrOut;
 };
 
-//Test Code
-//console.log("1st test:",middle([1, 2, 3]));
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
 
---
+++ test/middleTest.js
@@ -0,0 +1,14 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require('../middle');
+
+//Test Code
+//console.log("1st test:",middle([1, 2, 3]));
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
